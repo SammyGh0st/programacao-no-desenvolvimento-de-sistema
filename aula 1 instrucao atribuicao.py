c5d7c7cd14747eb1fe4ac42d6e9fdc6923eae8d6
SammyGh0st <gabriella.silva30@escola.pr.gov.br>
2026-03-09 11:13:17 -0300

    Add files via upload

---
+++ aula 1 instrucao atribuicao.py
@@ -0,0 +1,11 @@
+#variavel é um espaço de memoria que
+# tem um nome e guarda algum dado do meu programa
+numero = 1
+
+#expressoes sao qualquer combinacoes de
+#valor, variavel e apoeradores
+resultado = numero + numero + 5
+resultado = (numero * (numero * numero) + 33)
+
+# a instrucao print mostra o
+print(numero)
